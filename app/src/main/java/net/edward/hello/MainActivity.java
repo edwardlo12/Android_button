9dad7c4f3ff39d8d3dc7c9bd1cc9033eb14000a3
edwardlo12 <edward70932@gmail.com>
2017-04-06 21:00:38 +0800

    kkkk

---
+++ app/src/main/java/net/edward/hello/MainActivity.java
@@ -0,0 +1,33 @@
+package net.edward.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textStatus;
+    private Button button;
+    int a = Integer.MAX_VALUE - 5;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textStatus = (TextView) findViewById(R.id.textView2);
+        button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                a++;
+                textStatus.setText("你要付我"+a+"科威特幣");
+            }
+        });
+    }
+}
