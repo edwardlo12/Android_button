671af6b9f42bc7b7502d0dd159782f48a280f44f
edwardlo12 <edward70932@gmail.com>
2017-04-20 20:51:38 +0800

    2.1

--- app/src/main/java/net/edward/hello/MapsActivity.java
+++ app/src/main/java/net/edward/hello/MapsActivity.java
@@ -2,26 +2,33 @@ package net.edward.hello;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.*;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnCameraIdleListener{
 
     private GoogleMap mMap;
+    private TextView mMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mMessage = (TextView)findViewById(R.id.message);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
     }
 
 
@@ -37,10 +44,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnCameraIdleListener(this);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng FCU_IEE = new LatLng(24.179111, 120.649451);
+        //mMap.addMarker(new MarkerOptions().position(FCU_IEE).title("逢甲大學資電館"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(FCU_IEE,19));
+    }
+
+    @Override
+    public void onCameraIdle() {
+        mMessage.setText("camera move ! ");
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        mMessage.setText("click !  point=" + latLng);
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        mMessage.setText("long click !  point=" + latLng);
     }
 }
