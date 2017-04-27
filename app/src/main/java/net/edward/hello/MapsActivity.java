49ac96554972463109454fcf0687a0cef1b66edd
edwardlo12 <edward70932@gmail.com>
2017-04-27 23:42:07 +0800

    3.0

--- app/src/main/java/net/edward/hello/MapsActivity.java
+++ app/src/main/java/net/edward/hello/MapsActivity.java
@@ -1,8 +1,19 @@
 package net.edward.hello;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,12 +21,20 @@ import com.google.android.gms.maps.GoogleMap.*;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnCameraIdleListener{
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,LocationListener,GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnCameraIdleListener{
 
     private GoogleMap mMap;
     private TextView mMessage;
+    Marker currentMarker;
+    LatLng prevLatLng;
+    LocationManager lms;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +45,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        LocationManager status = (LocationManager) (this.getSystemService(Context.LOCATION_SERVICE));
+        if (status.isProviderEnabled(LocationManager.GPS_PROVIDER) || status.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            lms = (LocationManager) getSystemService(LOCATION_SERVICE); //change
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            }
+            Location location = lms.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            lms.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000,0,this);
+            if(mMap != null){
+                setCamera(location);
+                setMarker(location);
+                setPolyLine(location);
+            }
 
 
 
+        } else {
+            Toast.makeText(this, "請開啟定位服務", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
+        }
     }
 
 
@@ -44,13 +84,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         mMap.setOnMapClickListener(this);
         mMap.setOnMapLongClickListener(this);
         mMap.setOnCameraIdleListener(this);
 
         LatLng FCU_IEE = new LatLng(24.179111, 120.649451);
         //mMap.addMarker(new MarkerOptions().position(FCU_IEE).title("逢甲大學資電館"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(FCU_IEE,19));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(FCU_IEE,15));
     }
 
     @Override
@@ -67,4 +108,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapLongClick(LatLng latLng) {
         mMessage.setText("long click !  point=" + latLng);
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+            if(mMap != null){
+                setCamera(location);
+                setMarker(location);
+                setPolyLine(location);
+            }
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+    private void setMarker(Location location) {
+        LatLng current = new LatLng(location.getLatitude(), location.getLongitude());
+        if(currentMarker == null){
+            currentMarker = mMap.addMarker(new MarkerOptions().position(current).
+                    title("Lat: " + location.getLatitude() +
+                            " Long:" + location.getLongitude()));
+        }else{
+            currentMarker.setPosition(current);
+            currentMarker.setTitle("Lat: " + location.getLatitude() +
+                    " Long:" + location.getLongitude());
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+    }
+
+    private void setCamera(Location location){
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(
+                new LatLng(location.getLatitude(),location.getLongitude())));
+    }
+
+    private void setPolyLine(Location location){
+        if(prevLatLng == null){
+            prevLatLng = new LatLng(location.getLatitude(),location.getLongitude());
+        }else{
+            LatLng currentLatLng = new LatLng(location.getLatitude(),location.getLongitude()) ;
+            mMap.addPolyline(new PolylineOptions()
+                    .add(prevLatLng, currentLatLng).width(5).color(Color.BLUE));
+        }
+    }
+
+
 }
